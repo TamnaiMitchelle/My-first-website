a0137b8c24cfeda6936654c9c2e5ace72d178508
Tamnai Mitchelle <mitchellecheptoek@gmail.com>
2023-12-07 14:16:00 +0300

    Modified file styles.css

---
+++ app.js
@@ -0,0 +1,13 @@
+//
+console.log("Hello my friend")
+console.log("Tamnai says \'good\'")
+console.log("Tamnai says \'AMAZING\'")
+let age = 23
+var name = ('Your age is ${age}');
+console.log("Your age is", age)
+
+let new1
+console.log(new1)
+
+let weight = 50.1
+let signup = true
